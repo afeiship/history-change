6a2862dc660acbb46182c2c9d26360d57e910355
feizheng <1290657123@qq.com>
2016-09-24 01:04:02 +0800

    initial

---
+++ index.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+## basic alias:
+alias bi='bower install';
+alias bi-s='bower install --save';
+alias bi-d='bower install --save-dev';
---
+++ initialize.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+npm install bower -g;
