8dbe76dd2f1efd31c39054cd163ed64a60663677
feizheng <1290657123@qq.com>
2016-09-25 11:00:02 +0800

    update bower

--- index.sh
+++ index.sh
@@ -6,3 +6,5 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias bi='bower install';
 alias bi-s='bower install --save';
 alias bi-d='bower install --save-dev';
+alias bi-clean='bower cache clean';
+alias bi-list='bower cache list';
